ddf57f3cb0549b2f67e34c6fb5142a79dcd9d67f
Horiz21 <horizoi@outlook.com>
2023-03-16 12:29:41 +0800

    增加英语文件

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,10 +8,6 @@ using System.Windows;
 
 namespace LambdaLauncher
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
-	public partial class App : Application
-	{
+	public partial class App : Application {
 	}
 }
