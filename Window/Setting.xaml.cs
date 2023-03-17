62dc9807331aaa838ce02eb4af7058b794f6b93d
Horiz21 <horizoi@outlook.com>
2023-03-17 11:02:35 +0800

    暂时语言切换功能

--- Window/Setting.xaml.cs
+++ Window/Setting.xaml.cs
@@ -1,36 +1,38 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Windows;
 using System.Xml.Linq;
 
 namespace LambdaLauncher {
 	public partial class Setting : Window {
+		private Dictionary<string, string> LanguageDictionary = new Dictionary<string, string> {
+			{"中文(中国)","zh_cn"},
+			{"English","en_us"},
+		};
+
 		public Setting() {
 			InitializeComponent();
 		}
 
 		private void ChangeLanguage(object sender, RoutedEventArgs e) {
-			string appXamlPath = System.IO.Path.GetFullPath(@"..\..\..\App.xaml");
-			XDocument appXaml = XDocument.Load(appXamlPath);
 
-			XNamespace x = "http://schemas.microsoft.com/winfx/2006/xaml";
-
-			// 找到当前语言的字典路径
-			var currentLanguageDict = appXaml.Descendants(x + "ResourceDictionary")
-				.Where(d => d.Attribute("x:Name")?.Value == "CurrentLanguage")
-				.FirstOrDefault();
-
-			// 若存在这条路径（通常情况下都是存在的），则修改原始路径，然后写回
-			if (currentLanguageDict != null) {
-				var sourceAttr = currentLanguageDict.Attribute("Source");
-				if (sourceAttr != null) {
-					sourceAttr.Value = "pack://application:,,,/Language/en_us.xaml";
-				}
-			}
-			appXaml.Save(appXamlPath);
 		}
 
 		private void DragWindow(object sender, System.Windows.Input.MouseButtonEventArgs e) => DragMove();
 
 		private void CloseWindow(object sender, RoutedEventArgs e) => Close();
+
+		private void TempChangeLanguage(object sender, System.Windows.Controls.SelectionChangedEventArgs e) {
+			int index = boxLanguage.SelectedValue.ToString().IndexOf(' ');
+			if (index != -1) {
+				string language = boxLanguage.SelectedValue.ToString().Substring(index+1);
+				if (language != string.Empty) {
+					string head = @"pack://application:,,,/";
+					App.Current.Resources.MergedDictionaries[0].Source = new Uri(head + "Language/" + LanguageDictionary[language] + ".xaml");
+				}
+			}
+		}
 	}
 }
